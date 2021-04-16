1009cc7872176a135c9b638ecf31c67b19d54100
Mehrojbek <mehrojbek970510@gmail.com>
2021-04-16 22:38:39 +0500

    third commit

--- src/main/java/uz/pdp/apppcmarket/repository/ProductRepository.java
+++ src/main/java/uz/pdp/apppcmarket/repository/ProductRepository.java
@@ -1,10 +1,21 @@
 package uz.pdp.apppcmarket.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import uz.pdp.apppcmarket.entity.Product;
 import uz.pdp.apppcmarket.projection.CustomProduct;
 
+import java.util.*;
+
 @RepositoryRestResource(path = "product",excerptProjection = CustomProduct.class)
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+    @RestResource(path = "byProperty")
+    @Query(value = "select * from product p join characteristics ch on p.id=ch.product_id " +
+            "join property pr on ch.id=pr.characteristic_id where pr.name=:name",
+            nativeQuery = true)
+    List<Product> getAllByProperty(String name);
+
 }
